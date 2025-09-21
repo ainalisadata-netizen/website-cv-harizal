3b48308f37be7d873a4c1c4d60f16f669786d60e
ainalisadata-netizen <ainalisadata@gmail.com>
2025-09-21 10:16:40 +0700

    fixing server.js 06

--- server.js
+++ server.js
@@ -80,7 +80,6 @@ createFirstAdmin();
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT, 10),
-    secure: process.env.EMAIL_SECURE === 'true',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
